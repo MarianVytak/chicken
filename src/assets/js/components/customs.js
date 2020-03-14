cd1d7e90946f9100e138bdeb0b14fdf90fae0900
MarianVytak <marianvytak@gmail.com>
2020-03-14 16:07:30 +0200

    Added Contacts and Modals.

--- src/assets/js/components/customs.js
+++ src/assets/js/components/customs.js
@@ -2,4 +2,28 @@ $(function () {
 
     'use strict';
 
+    // Navigation anchors
+    $('.js-anchor').on('click', function(e){
+        e.preventDefault();
+
+        let navScroll = $(this).attr('href'),
+            navScrollBlock = $(navScroll).offset().top;
+        $('html, body').animate({
+            scrollTop: navScrollBlock
+        }, 2000);
+    });
+
+
+    // Modal
+    $('.btn-modal-call').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').addClass('modal-active');
+        $('.modal-call').fadeIn();
+    });
+    $('.modal-close').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').removeClass('modal-active');
+        $('.modal-call').fadeOut();
+    });
+
 });
